8d54af3baba07466cdb5be31b6a75972898cf3ca
League Student <league@iMac-40.attlocal.net>
2019-08-04 12:38:27 -0700

    Fixed all the bugs
    sadsd

--- src/CommentReader/CommentReader.java
+++ src/CommentReader/CommentReader.java
@@ -1,4 +1,5 @@
 package CommentReader;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -9,6 +10,8 @@ import java.util.ArrayList;
 
 public class CommentReader {
 	ArrayList<String> keep;
+	ArrayList<Integer> number;
+	ArrayList<String> filenames;
 
 	public static void main(String[] args) {
 		// TODO
@@ -20,56 +23,79 @@ public class CommentReader {
 
 	// TODO
 	void activate() {
+		filenames = new ArrayList<String>();
 		String tot = "";
+		int frt = 0;
 		keep = new ArrayList<String>();
+		number = new ArrayList<Integer>();
 		scan();
+
 		System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO" + keep.size());
-		for (int i = 0; i < keep.size(); i++) { 
-			tot = tot + keep.get(i) + "\n";
+		tot = tot + filenames.get(frt) + "\n";
+		frt++;
+		for (int i = 0; i < keep.size(); i++) {
+			tot = tot + keep.get(i) + " " + number.get(i) + "\n";
+			if (i > 0) {
+				if (number.get(i) < number.get(i - 1)) {
+					tot = tot + filenames.get(frt) + "\n";
+					frt++;
+				}
+			}
+
 		}
 		System.out.println(tot);
-			try {
-				FileWriter fw = new FileWriter("src/CommentReader/Comment");
-				
-				fw.write(tot);
-				fw.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		
+		try {
+			FileWriter fw = new FileWriter("src/CommentReader/Comment", false);
+
+			fw.write(tot);
+			fw.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	void scan() {
-		
-		System.out.println("Did Scan");
+
 		File TakoEngine = new File("src/CommentReader");
 		File[] directoryListing = TakoEngine.listFiles();
 		if (directoryListing != null) {
 			for (File child : directoryListing) {
-				try {
-					BufferedReader br = new BufferedReader(new FileReader(child));
-					String line = br.readLine();
-					while (line != null) {
-						line = line.trim();
-						if (line.length() > 3) {
-							
-							System.out.println(line);
-							// TODO make sure it scans words right
-							if (line.substring(0, 4).equals("// T")) {
-								keep.add(line);
-								System.out.println("YAAYY");
+				if (child.getAbsolutePath()
+						.substring(child.getAbsolutePath().length() - 4, child.getAbsolutePath().length())
+						.equals("java")) {
+					filenames.add(child.getAbsolutePath());
+					try {
+						int i = 0;
+						BufferedReader br = new BufferedReader(new FileReader(child));
+						String line = br.readLine();
+						while (line != null) {
+
+							i++;
+
+							line = line.trim();
+							if (line.length() > 3) {
+
+								// TODO make sure it scans words right
+								if (line.substring(0, 4).equals("// T")) {
+									keep.add(line);
+									number.add(i);
+
+								}
+
 							}
-							
+							line = br.readLine();
+
 						}
-						line = br.readLine();
+						br.close();
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
-					br.close();
-				} catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
 				}
 			}
 		} else {
